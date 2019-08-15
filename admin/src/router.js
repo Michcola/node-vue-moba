49c4359df79a70cf526d408a72d3fefd5c44bb26
mich <843298603@qq.com>
2019-08-15 19:45:27 +0800

    添加物品管理

--- admin/src/router.js
+++ admin/src/router.js
@@ -5,6 +5,8 @@ import Main from './views/Main.vue'
 import CategoryEdit from './views/CategoryEdit.vue'
 import CategoryList from './views/CategoryList.vue'
 
+import ItemEdit from './views/ItemEdit.vue'
+import ItemList from './views/ItemList.vue'
 
 
 
@@ -20,6 +22,10 @@ export default new Router({
         { path: '/categories/create', component: CategoryEdit },
         { path: '/categories/edit/:id', component: CategoryEdit, props: true },
         { path: '/categories/list', component: CategoryList },
+
+        { path: '/items/create', component: ItemEdit },
+        { path: '/items/edit/:id', component: ItemEdit, props: true },
+        { path: '/items/list', component: ItemList },
       ]
     },
     

f57d1eed81323dbeffb3092eb204f9a6e140d52c
mich <843298603@qq.com>
2019-08-16 23:18:46 +0800

    登录接口

--- admin/src/http.js
+++ admin/src/http.js
@@ -1,7 +1,21 @@
 import axios from 'axios'
+import Vue from "vue";
 
 const http = axios.create({
     baseURL: 'http://localhost:3000/admin/api'
+})
+//在前端封装通用的错误处理接口
+http.interceptors.response.use(res => {
+    return res
+}, err => {
+    if (err.response.data.message) {
+        Vue.prototype.$message({
+            type: 'error',
+            message: err.response.data.message
+        })
+    }
+
+    return Promise.reject(err)
 })
 
 export default http

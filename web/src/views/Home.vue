5d7e045544a15e38129898e83544dfadb37fb7ce
mich <843298603@qq.com>
2019-08-19 17:46:08 +0800

    英雄详情页-前端准备

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -29,11 +29,13 @@
 
     <m-list-card icon="menu1" title="新闻资讯" :categories="newsCats">
       <template #items="{category}">
-        <router-link 
-        tag="div"
-        :to="`/articles/${news._id}`"
-        class="py-2 fs-lg d-flex" 
-        v-for="(news, i) in category.newsList" :key="i">
+        <router-link
+          tag="div"
+          :to="`/articles/${news._id}`"
+          class="py-2 fs-lg d-flex"
+          v-for="(news, i) in category.newsList"
+          :key="i"
+        >
           <span class="text-info-1">[{{news.categoryName}}]</span>
           <span class="px-2">|</span>
           <span class="flex-1 text-dark-1 text-ellipsis pr-2">{{news.title}}</span>
@@ -45,12 +47,17 @@
     <m-list-card icon="card-hero" title="英雄列表" :categories="heroCats">
       <template #items="{category}">
         <div class="d-flex flex-wrap" style="margin: 0 -0.5rem;">
-          <div class="p-2 text-center" 
-          style="width: 20%;"
-          v-for="(hero, i) in category.heroList" :key="i">
+          <router-link
+            tag="div"
+            :to="`/heroes/${hero._id}`"
+            class="p-2 text-center"
+            style="width: 20%;"
+            v-for="(hero, i) in category.heroList"
+            :key="i"
+          >
             <img :src="hero.avatar" class="w-100" />
             <div>{{hero.name}}</div>
-          </div>
+          </router-link>
         </div>
       </template>
     </m-list-card>
